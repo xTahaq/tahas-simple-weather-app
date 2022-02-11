6b97619e0f4182f7f12453adeb146ae1890fa97b
xTahaq <63209085+xTahaq@users.noreply.github.com>
2022-02-11 15:02:22 +0300

    epic update
    - now made in tkinter
    - search a place and it will give a text output (i was too lazy to finish forecast part so i turned the json to string i will fix it in next update)
    - there's now settings json and if  you go to settings.json you can change degree type to F or C (C as default) - made it so if you try typing something else it will use default one
    - there will be console at the background for now, for debugging purposes

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 import requests
 import xmltodict
 import json
-###import tkinter
+from tkinter import *
 
-########### THIS APP IS IN EARLY DEMO - TKINTER (GUI) VERSION COMING VERY SOON. #################
-########### THIS APP IS IN EARLY DEMO - TKINTER (GUI) VERSION COMING VERY SOON. #################
-########### THIS APP IS IN EARLY DEMO - TKINTER (GUI) VERSION COMING VERY SOON. #################
-########### THIS APP IS IN EARLY DEMO - TKINTER (GUI) VERSION COMING VERY SOON. #################
-# this app is so simple but i was out of ideas dont bully me lmaoo tkinter will make it look more complicated tho so no problem
+########### THIS APP IS IN EARLY DEMO ###############
+########### THIS APP IS IN EARLY DEMO ###############
+########### THIS APP IS IN EARLY DEMO ###############
+# this app is so simple but i was out of ideas dont bully me lmaoo
 
+with open("settings.json", "r") as json_file:
+    data = json.load(json_file)
+
+
+#def updateFile():
+#    with open("settings.json", "w") as json_file:
+#        json.dump(data, json_file, indent=4, sort_keys=False)
 
 def getNextDayForecast(inp):
     days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
@@ -40,6 +46,9 @@ def simplifyResults(inp):
         }
 
 def searchForPlace(toSearch, degreeType="C"):
+    print(degreeType)
+    if degreeType != "C" and degreeType != "F":
+        degreeType = "C"
     resp = requests.get("https://weather.service.msn.com/find.aspx?src=outlook&weadegreetype=" + degreeType + "&culture=en&weasearchstr=" + toSearch)
     try:
         dict_data = xmltodict.parse(resp.content)
@@ -56,13 +65,63 @@ def searchForPlace(toSearch, degreeType="C"):
 
 ##STARTUP
 
-print("WELCOME TO THE DEMO WEATHER APP, TYPE A PLACE NAME TO SEARCH.")
-while True:
-    place = input("Type a place >>> ")
-    if place == "EXIT": exit()
-    res = searchForPlace(place)
-    #res = searchForPlace(place, "F")
-    if res == 0:
-        print("Place not found. Try something else!\n")
-    else:
+GUI = Tk()
+GUI.title("HELLO")
+GUI.minsize(500, 500)
+
+guigap = Label(GUI, text=" \n ")
+guigap.pack()
+mf = Frame(GUI) # main frame
+mf.pack(side=TOP, fill=X)
+gf = Frame(GUI, bd=0) # gridded frame
+gf.pack(side=TOP, fill=X)
+
+def search():
+    label = Label(mf, text="Please enter a place name to search for weather:")
+    label.pack()
+    entry = Entry(mf)
+    entry.pack()
+    Label(mf, text="").pack() #space
+
+    def buttonFunc():
+        print("Recieved", entry.get())
+        res = searchForPlace(entry.get(), data["degreeType"])
         print(json.dumps(res, indent=4, sort_keys=True))
+        if res == 0:
+            output["text"] = "Place not found"
+        else:
+            output["text"] = ""
+            somelabel = Label(mf, text="Please click one of the places to see the weather:")
+            somelabel.pack()
+            arrOfButts = []
+            def turnDataToString(index):
+                d = res[index]
+                op = ""
+                for i in arrOfButts:
+                    i.destroy()
+                somelabel.destroy()
+                op = op + "Place Name: " + d["locationname"] + "\n"
+                op = op + "Temperature: " + d["temperature"] + " " + data["degreeType"] + "\n"
+                op = op + "Status: " + d["skytext"] + "\n"
+                op = op + "Wind: " + d["wind"] + "\n"
+                op = op + "Humidity: " + d["humidity"] + "%\n"
+                op = op + "Tomorrows Forecast:" + str(d["forecast"]) + "\n"
+                output["text"] = op
+                
+            indx = 0
+            for i in res:
+                b = Button(mf, text=i["locationname"], command=lambda indx=indx: turnDataToString(indx))
+                b.pack()
+                arrOfButts.append(b)
+                indx += 1
+
+
+    button = Button(mf, text="ENTER", command=buttonFunc)
+    button.pack()
+    Label(mf, text="").pack() #space
+    output = Label(mf, text="")
+    output.pack()
+
+search()
+
+GUI.mainloop()
